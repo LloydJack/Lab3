475eb195c296ad1f3fdaa434e348dc30770afa64
1303788 <lloydjack1@hotmail.co.uk>
2015-10-19 16:26:33 +0100

    Yo

--- index.php
+++ index.php
@@ -75,7 +75,17 @@
                     print "<p>$x</p>";
                 }
 
+                $provisionedActivities[1] = "hugs";
 
+                foreach($provisionedActivities as $x) {
+                    print "<p>$x</p>";
+                }
+
+                unset($provisionedActivities[2]);
+
+                foreach($provisionedActivities as $x) {
+                    print "<p>$x</p>";
+                }
 
 
 
