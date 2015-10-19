21c1bec7b60c53bfc69f03620acf64c7f6538d53
1303788 <lloydjack1@hotmail.co.uk>
2015-10-19 15:34:29 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1303788
+ * Date: 19/10/2015
+ * Time: 15:27
+ */
+
+<!DOCTYPE html>
+<HTML>
+    <head>
+        </head>
+        <body>
+            <p>
+                <?php
+                    echo "Hello World" ;
+                ?>
+            </p>
+        </body>
+</HTML>
